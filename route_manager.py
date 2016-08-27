cfcf58ca8633d297ea1af3255d83a8f77a7b40a0
Jbaldy16 <Jbaldy16@users.noreply.github.com>
2016-08-27 07:25:26 -0400

    Version Update

--- route_manager.py
+++ route_manager.py
@@ -6,25 +6,37 @@ def addRoute(startLoc, endLoc, routeName, activate=False):
 	end_location = session.query(Locations).filter(Locations.name == endLoc).one()
 
 	# Check to see if it exists
-    check = session.query(exists().where(Routes.start_location_id == start_location.id) \
-        .where(Routes.end_location_id == end_location.id).scalar()
+    check_locs = session.query(exists().where(Routes.start_location_id == start_location.id) \
+        .where(Routes.end_location_id == end_location.id).scalar())
 
-    if check:
+    checkname = session.query(exists().where(Routes.name == routeName).scalar())
+
+    if checklocs:
     	print "Route Already Exists"
     	break
-
-	new_route = Routes(start_location_id=start_location.id, end_location_id=end_location.id, \
-		name=routeName, active=activate)
-    session.add(new_route)
-    session.commit()
+    elif checkname:
+        print "Route Name Already Used"
+    	break
+    else:
+		new_route = Routes(start_location_id=start_location.id, end_location_id=end_location.id, \
+			name=routeName, active=activate)
+    	session.add(new_route)
+    	session.commit()
+    	print "Route Added"
 
 def printRoutes():
 	routes = session.query(Routes).all()
 	for route in routes:
 		print 'Route id:', route.id, 'Name:', route.name, 'Active?', route.active
 
-def activateRoute():
-	pass
+def activateRoute(routeName=None):
+	if routeName==None:
+		print "No Route Provided"
+		break
+	else:
+		checkname = session.query(exists().where(Routes.name == routeName).scalar())
+		if checkname:
+			route = session.query(Routes).filter(Routes.name==routeName).one()
+			route.activate = True
+			session.commit()
 
-def processRoutes():
-	pass
